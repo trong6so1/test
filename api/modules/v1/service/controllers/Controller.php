bf4f68b8fa9cf9aef4add94eb8761a95c2f401e2
trong6so1 <trong6so1@gmail.com>
2024-05-16 02:36:06 -0700

    Determine how requests and access permissions are handled in the create service action

---
+++ api/modules/v1/service/controllers/Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace api\modules\v1\service\controllers;
+
+use yii\filters\AccessControl;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+
+class Controller extends \yii\rest\Controller
+{
+    public function verbs(): array
+    {
+        return [
+            'create' => ['POST'],
+        ];
+    }
+
+    public function behaviors(): array
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::class,
+            'authMethods' => [
+                HttpBearerAuth::class,
+            ]
+        ];
+        $behaviors['access'] = [
+            'class' => AccessControl::class,
+            'rules' => [
+                [
+                    'allow' => true,
+                    'actions' => ['create'],
+                    'roles' => ['administrator'],
+                ],
+            ],
+        ];
+        return $behaviors;
+    }
+}
