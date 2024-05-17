72c6bc173ea3f8c724485c7158b2735186f13bfe
trong6so1 <trong6so1@gmail.com>
2024-05-17 00:58:44 -0700

    Display Report and export Excel Report

---
+++ api/modules/v1/report/controllers/IncomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace api\modules\v1\report\controllers;
+
+use api\helper\response\ApiConstant;
+use api\helper\response\ResultHelper;
+use api\modules\v1\report\models\search\OrderPaymentMethodSearch;
+use api\traits\ExportExcelTrait;
+use PhpOffice\PhpSpreadsheet\Writer\Exception;
+use Yii;
+
+class IncomeController extends Controller
+{
+    use ExportExcelTrait;
+
+    public function actionIndex(): array
+    {
+        $report = (new OrderPaymentMethodSearch())->search(Yii::$app->request->queryParams);
+        $statusCode = ApiConstant::STATUS_OK;
+        $data = $report;
+        $error = null;
+        $message = "Report income successfully";
+        return ResultHelper::build($statusCode, $data, $error, $message);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function actionExport(): array
+    {
+        $dataProvider = new OrderPaymentMethodSearch();
+        $fields = $dataProvider->fields();
+        $data = $dataProvider->search(Yii::$app->request->queryParams)->getModels();
+        $fileName = 'export_report_income_' . date('YmdHis') . '.xlsx';
+        $fileDir = Yii::getAlias('@app/export/');
+        if (!is_dir($fileDir)) {
+            mkdir($fileDir, 0777, true);
+        }
+        $filePath = $fileDir . $fileName;
+        return $this->exportExcel($data, $filePath, $fileName, $fields);
+    }
+}
