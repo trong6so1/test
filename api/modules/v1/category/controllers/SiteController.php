eb56f7d59dbcb5bb3a416d9a830ca4f6f950503c
trong6so1 <trong6so1@gmail.com>
2024-05-17 02:06:19 -0700

    Edit data transmitted into excel export traits file

--- api/modules/v1/category/controllers/SiteController.php
+++ api/modules/v1/category/controllers/SiteController.php
@@ -60,10 +60,6 @@ class SiteController extends Controller
         $data = (new CategorySearch())->search(Yii::$app->request->queryParams)->getModels();
         $fileName = 'export_category_' . date('YmdHis') . '.xlsx';
         $fileDir = Yii::getAlias('@app/export/');
-        if (!is_dir($fileDir)) {
-            mkdir($fileDir, 0777, true);
-        }
-        $filePath = $fileDir . $fileName;
-        return $this->exportExcel($data, $filePath, $fileName);
+        return $this->exportExcel($data, $fileDir, $fileName);
     }
 }
